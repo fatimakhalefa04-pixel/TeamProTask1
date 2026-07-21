fbff781b7de42f128c8326f3405b48da773f8f0c
fatimakhalefa04-pixel <fatimakhalefa04@gmail.com>
2026-07-21 22:19:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-<<<<<<< HEAD
         System.out.println("Hello");
-=======
         System.out.println("");
         System.out.println("shahedeleid");
->>>>>>> origin/shahed26
     }
 }
